9754c64d6922751d00012201508364f9f62f9076
obinnangini <obinnangini@gmail.com>
2017-04-03 07:50:21 -0400

    Removed unecessary imports from main class

--- Interpreter.rb
+++ Interpreter.rb
@@ -6,27 +6,7 @@ Using the Object Oriented Approach, each non terminal has a class that contains 
 A tokenizer is implemented to gather valid tokens for Core and parse any Core program effectively 
 =end
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-require_relative './model/ID'
-require_relative './model/ID_List'
-require_relative './model/Comparison_Op'
-require_relative './model/Operator'
-require_relative './model/Term'
-require_relative './model/Exp'
-require_relative './model/Comparator'
-require_relative './model/Condition'
-require_relative './model/Out'
-require_relative './model/In'
-require_relative './model/If'
-require_relative './model/Loop'
-require_relative './model/Stmt_Seq'
-require_relative './model/Stmt'
-require_relative './model/Assign'
-require_relative './model/Decl'
-require_relative './model/Decl_Seq'
 require_relative './model/Program'
-
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class Interpreter
   def interpretprog
